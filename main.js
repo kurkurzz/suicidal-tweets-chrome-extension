444362afdc4e6b8301be0569a86e251bf1d349c3
Muhammad Nurhafiz <64152220+kurkurzz@users.noreply.github.com>
2022-06-05 02:29:34 +0800

    label depression

--- main.js
+++ main.js
@@ -5,9 +5,16 @@ console.log("Initializing suicidal tweets detector!");
 
 backend_url = "https://suicidal-chrome-extension.herokuapp.com/classify?text="
 
+var head  = document.getElementsByTagName('head')[0];
+var link  = document.createElement('link');
+link.rel  = 'stylesheet';
+link.type = 'text/css';
+link.href = './style.css';
+link.media = 'all';
+head.appendChild(link);
+
 function checkTweets() {
 	let tweets = document.querySelectorAll("article");
-
 	tweets.forEach(function (tweet) {
 		try {
 			text = tweet.querySelector('[data-testid="tweetText"]').innerText;
@@ -16,10 +23,13 @@ function checkTweets() {
 				.then((data) => {  
 					console.log(data)      
 					if (data["result"] == "suicidal"){
-						tweet.setAttribute(
-								"style",
-								"background-color: rgb(255, 134, 134);"
-							);
+						const check_label = tweet.getElementsByClassName("depressionLabel")[0]
+						if(typeof(check_label) == "undefined" || check_label == null){
+							const label = document.createElement("span")
+							label.className = "depressionLabel"
+							label.innerHTML = "depression"
+							tweet.appendChild(label)
+						}
 					}
 				})
 				.catch(e=>console.error(e));
